c33c7e935a3aa0d4561dc254023f85a34075d8ca
Zug <2681491l@student.gla.ac.uk>
2022-02-28 21:32:25 +0000

    检测单位是否满足放置条件

--- AIPlayer.java
+++ AIPlayer.java
@@ -20,29 +20,35 @@ public class AIPlayer extends Player {
 	private HashMap<Unit, ArrayList<Integer>> enemyUnitPos; // 敌方场上单位id及其坐标
 	private HashMap<Unit, ArrayList<Integer>> friendlyUnitPos; // 我方场上单位id及其坐标
 	private List<Card> hand;
+	private int i;
+	private int j;
 
 	public void doSomething(ActorRef out, GameState gameState) {
 		// AI player will do something to modify its cards and units
 		this.AIHand(gameState);
-		
-		//get enemy and friendly units and their positions
+
+		// get enemy and friendly units and their positions
 		this.getEnemyUnitOnBoard(gameState);
 		this.getFriendlyUnitOnBoard(gameState);
-		
-		//get enemy and friendly avatars and their positions
+
+		// get enemy and friendly avatars and their positions
 		this.getEnemyAvatarOnBoard(gameState);
 		this.getFriendlyAvatarOnBoard(gameState);
-		
-		//play a card on a certain tile
-		int i = 7, j = 3;
-		Card c1 = this.hand.get(0);
-		System.out.println(c1.getCardName() + c1.getId());
-		this.placeAUnit(gameState, out, i, j, c1);
-		
-		Card c2 = this.hand.get(0);
-		System.out.println(c2.getCardName() + c2.getId());
-		this.placeAUnit(gameState, out, i-1, j-1, c2);
 
+		// play a card on a certain tile
+		i = 7;
+		j = 3;
+		this.checkUnitCostAndPlace(gameState, hand.get(0), out, i, j);
+		
+		
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		// AI finish its round, add 1 to round number
 		switchToHumanPlayer(out, gameState);
 	}
@@ -77,30 +83,30 @@ public class AIPlayer extends Player {
 
 			this.enemyUnitPos.put(u, pos);
 		}
-		
-		//for test
+
+		// for test
 		System.out.println("Enemy Unit Positions:");
-		for(Map.Entry<Unit, ArrayList<Integer>> entries : enemyUnitPos.entrySet()){
-			System.out.println(entries.getKey().getId() + ": " + entries.getValue() );
+		for (Map.Entry<Unit, ArrayList<Integer>> entries : enemyUnitPos.entrySet()) {
+			System.out.println(entries.getKey().getId() + ": " + entries.getValue());
 		}
 	}
 
 	private ArrayList<Integer> getEnemyAvatarOnBoard(GameState gs) {
 		ArrayList<Integer> temp = null;
-		for(Map.Entry<Unit, ArrayList<Integer>> entries : enemyUnitPos.entrySet()) {
-			if(entries.getKey().getId() == 40) { // the id of enemy avatar is 40
+		for (Map.Entry<Unit, ArrayList<Integer>> entries : enemyUnitPos.entrySet()) {
+			if (entries.getKey().getId() == 40) { // the id of enemy avatar is 40
 				temp = entries.getValue();
 			}
 		}
-		//for test
+		// for test
 		System.out.print("Enemy Avatar Position is: ");
-		for(Integer i : temp) {
+		for (Integer i : temp) {
 			System.out.print(i + " ");
 		}
 		System.out.println();
 		return temp;
 	}
-	
+
 	private void AIHand(GameState gameState) {
 		hand = gameState.aiPlayer.getHand();
 	}
@@ -115,25 +121,25 @@ public class AIPlayer extends Player {
 
 			this.friendlyUnitPos.put(u, pos);
 		}
-		
-		//for test
+
+		// for test
 		System.out.println("Friendly Unit Positions: ");
-		for(Map.Entry<Unit, ArrayList<Integer>> entries : friendlyUnitPos.entrySet()){
-			System.out.println(entries.getKey().getId() + ": " + entries.getValue() );
+		for (Map.Entry<Unit, ArrayList<Integer>> entries : friendlyUnitPos.entrySet()) {
+			System.out.println(entries.getKey().getId() + ": " + entries.getValue());
 		}
-		
+
 	}
-	
+
 	private ArrayList<Integer> getFriendlyAvatarOnBoard(GameState gs) {
 		ArrayList<Integer> temp = null;
-		for(Map.Entry<Unit, ArrayList<Integer>> entries : friendlyUnitPos.entrySet()) {
-			if(entries.getKey().getId() == 41) { // the id of friendly avatar is 40
+		for (Map.Entry<Unit, ArrayList<Integer>> entries : friendlyUnitPos.entrySet()) {
+			if (entries.getKey().getId() == 41) { // the id of friendly avatar is 40
 				temp = entries.getValue();
 			}
 		}
-		//for test
+		// for test
 		System.out.print("Friendly Avatar Position is: ");
-		for(Integer i : temp) {
+		for (Integer i : temp) {
 			System.out.print(i + " ");
 		}
 		System.out.println();
@@ -168,6 +174,31 @@ public class AIPlayer extends Player {
 		System.out.println(card.getCardname() + "is on board");
 		System.out.println(unit.getAbilities());
 	}
+	
+	// check if the target tile is summonable and if the mana is enough to summon the unit
+	private void checkUnitCostAndPlace(GameState gs, Card c, ActorRef out, int tilex, int tiley) {
+		int mana = gs.aiPlayer.getMana();
+		gs.selectedCard = c;
+		
+		//to know where the unit can be placed on
+		gs.gameBoard.calculateAvailableSummonPlace(gs);
+		
+		if(gs.gameBoard.getTile(tilex, tiley).isAllowSummon()) {
+			//if this tile is summonable
+			//to know whether the cost is larger than mana
+			if (c.getManacost() <= mana) {
+				this.placeAUnit(gs, out, tilex, tiley, c);
+				gs.aiPlayer.setMana(mana -= c.getManacost());
+				System.out.println(c.getCardName() + "is on board now!");
+			} else {
+				System.out.println("Not enough mana!");
+			}
+		}else {
+			System.out.println("This tile is not summonable!");
+		}
+			
+		gs.gameBoard.cleanBoardFlag();
+	}
 
 	// 将某个spell释放给某个单位 cast a spell to a unit
 	private void castSpell() {
@@ -175,8 +206,21 @@ public class AIPlayer extends Player {
 	}
 
 	// 指挥一个单位移动到目标坐标 move a unit to a tile
-	private void moveTo() {
+	private void moveTo(GameState gs, Unit u, ActorRef out, int tilex, int tiley) {
+		// play animation
+		BasicCommands.moveUnitToTile(out, gs.selectedUnit, gs.gameBoard.getTile(tilex, tiley));
 
+		// set target board occupied
+		gs.gameBoard.resetUnitTileOccupied(gs.selectedUnit, tilex, tiley);
+
+		// set unit position
+		gs.selectedUnit.setPositionByCoordination(tilex, tiley);
+
+		// decrease move chance
+		gs.selectedUnit.decreaseMoveCount();
+
+		// cancel selection
+		gs.selectedUnit = null;
 	}
 
 	// 计算应该走到那个砖块
